ec312fb1c8e9db924b9fe3eb73421d4421615b57
Elle <michelle.wee.kh@gmail.com>
2015-12-20 08:35:19 +0000

    created index and main css

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,41 +1,48 @@
 'use strict';
 
-var Users = require('../models/users.js');
+var Clicks = require("../models/clicks.js")
 
-function ClickHandler () {
+function clickHandler (db) {
 
-	this.getClicks = function (req, res) {
-		Users
-			.findOne({ 'github.id': req.user.github.id }, { '_id': false })
-			.exec(function (err, result) {
-				if (err) { throw err; }
 
-				res.json(result.nbrClicks);
-			});
+    this.getClicks = function (req, res) {
+
+        Clicks
+            .findOne({}, {'_id': false})
+            .exec(function (err, result){
+                if (err) {throw err;}
+                
+                if (result) {
+                    res.json(result);
+                } else {
+                    var newDoc = new Clicks({'clicks':0});
+                    newDoc.save(function (err, doc){
+                        if (err) {throw err;}
+                        
+                        res.json(doc);
+                    });
+                }
+            });
 	};
-
+	
 	this.addClick = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { $inc: { 'nbrClicks.clicks': 1 } })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
+	    Clicks
+	        .findOneAndUpdate({}, {$inc: {'clicks': 1 } })
+	        .exec(function (err, result){
+	            if (err) {throw err;}
+	            
+	            res.json(result);
+	        });
 	};
-
-	this.resetClicks = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { 'nbrClicks.clicks': 0 })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
-	};
-
+	this.resetClicks = function (req, res){
+	    Clicks
+	        .findOneAndUpdate({}, {'clicks': 0 })
+	        .exec(function (err, result){
+	            if (err) {throw err;}
+	            
+	            res.json(result);
+	        })
+	}
 }
 
-module.exports = ClickHandler;
+module.exports = clickHandler;
